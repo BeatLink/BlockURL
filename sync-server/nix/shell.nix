ec80ce7b3c38edb798c1c92c4f671fad52d9db1b
BeatLink <git@beatlink.simplelogin.com>
2026-04-01 08:23:30 -0500

    Update shell

--- sync-server/nix/shell.nix
+++ sync-server/nix/shell.nix
@@ -1,7 +1,14 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable") {} }:
+{
+    pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable") { },
+}:
 
 pkgs.mkShellNoCC {
-  packages = with pkgs; [
-    (python3.withPackages (ps: [ ps.flask ]))
-  ];
+    packages = with pkgs; [
+        (python3.withPackages (ps: [ ps.flask ]))
+    ];
+    shellHook = ''
+        echo "BlockURL dev shell ready."
+        echo "Start the server with:"
+        echo "  cd sync-server && DATABASE_PATH=blockurl.db python3 -m blockurl"
+    '';
 }
