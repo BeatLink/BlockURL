b9fcea8ef8d4f2b3659a48961d8284e8321221cf
BeatLink <git@beatlink.simplelogin.com>
2026-04-01 13:34:42 -0500

    Fix main

--- sync-server/blockurl/__main__.py
+++ sync-server/blockurl/__main__.py
@@ -3,24 +3,17 @@ from flask import Flask
 from .database import DatabaseManager
 from .views import index_bp, init_settings, init_urls
 
-def create_app():
-    database = DatabaseManager(
-        database_name=os.environ['DATABASE_PATH'], 
-        create_tables=True, 
-        initialize_settings=True
-    )
-    app = Flask(__name__)
-    app.register_blueprint(index_bp)
-    app.register_blueprint(init_settings(database))
-    app.register_blueprint(init_urls(database))
-    return app
+database = DatabaseManager(
+    database_name=os.environ['DATABASE_PATH'], 
+    create_tables=True, 
+    initialize_settings=True
+)
+app = Flask(__name__)
+app.register_blueprint(index_bp)
+app.register_blueprint(init_settings(database))
+app.register_blueprint(init_urls(database))
 
-def launch_app(app):
+if __name__ == '__main__':
     host = os.environ.get('BLOCKURL_HOST', '0.0.0.0')
     port = int(os.environ.get('BLOCKURL_PORT', 8000))
     app.run(host=host, port=port, debug=True)
-
-app = create_app()
-
-if __name__ == '__main__':
-    launch_app(app)
