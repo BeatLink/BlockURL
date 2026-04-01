8d10dc9df7dc208a6d137d909a4e7cdbadae4ab6
BeatLink <git@beatlink.simplelogin.com>
2026-04-01 07:20:20 -0500

    Remove Flask Classful Dependency

--- sync-server/app/blockurl/__main__.py
+++ sync-server/app/blockurl/__main__.py
@@ -1,18 +1,23 @@
 import os
 from flask import Flask
 from .database import DatabaseManager
-from .views import IndexView, SettingsView, UrlsView
+from .views import index_bp, init_settings, init_urls
 
-app = Flask(__name__)
-database = DatabaseManager(
-    database_name=os.environ['DATABASE_PATH'], 
-    create_tables=True, 
-    initialize_settings=True
-)
-IndexView.register(app)
-SettingsView.register(app, init_argument=database)
-UrlsView.register(app, init_argument=database)
+def create_app(database):
+    app = Flask(__name__)
+    app.register_blueprint(index_bp)
+    app.register_blueprint(init_settings(database))
+    app.register_blueprint(init_urls(database))
+    return app
 
-if __name__ == '__main__':
+def launch_app():
+    database = DatabaseManager(
+        database_name=os.environ['DATABASE_PATH'], 
+        create_tables=True, 
+        initialize_settings=True
+    )
+    app = create_app(database)
     app.run(host='0.0.0.0', port=8000, debug=True)
 
+if __name__ == '__main__':
+    launch_app()
