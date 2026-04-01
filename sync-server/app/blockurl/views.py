8786631410edbf70768104a7f80481a1b94a7b1d
BeatLink <git@beatlink.simplelogin.com>
2026-04-01 07:21:27 -0500

    Remove flask-classful dependency

--- sync-server/app/blockurl/views.py
+++ sync-server/app/blockurl/views.py
@@ -1,61 +1,58 @@
 import html
-from flask import jsonify, request, render_template
-from flask_classful import FlaskView, route
-
+from flask import Blueprint, jsonify, request, render_template
 
 # Index ----------------------------------------------------------------------------------------------------------------
-class IndexView(FlaskView):
-    route_base = '/'
+index_bp = Blueprint('index', __name__)
 
-    def index(self):
-        return render_template("index.html")
+@index_bp.route('/')
+def index():
+    return render_template("index.html")
 
 
 # Settings -------------------------------------------------------------------------------------------------------------
-class SettingsView(FlaskView):
-    default_methods = ['POST']
+settings_bp = Blueprint('settings', __name__, url_prefix='/settings')
 
-    def __init__(self, database):
-        self.database = database
+def init_settings(database):
+    @settings_bp.route('/all', methods=["GET"])
+    def all():
+        return jsonify(database.get_all_settings())
 
-    @route('/all', methods=["GET"])
-    def all(self):
-        return jsonify(self.database.get_all_settings())
-
-    @route('/get', methods=["POST"])
-    def get(self):
+    @settings_bp.route('/get', methods=["POST"])
+    def get():
         key = request.get_json()["key"]
-        return jsonify(self.database.get_setting(key))
+        return jsonify(database.get_setting(key))
 
-    @route('/set', methods=["POST"])
-    def post(self):
+    @settings_bp.route('/set', methods=["POST"])
+    def post():
         key = request.get_json()["key"]
         value = request.get_json()["value"]
-        return jsonify(self.database.set_setting(key, value))
+        return jsonify(database.set_setting(key, value))
+
+    return settings_bp
 
 
 # URLs -----------------------------------------------------------------------------------------------------------------
-class UrlsView(FlaskView):
+urls_bp = Blueprint('urls', __name__, url_prefix='/urls')
 
-    def __init__(self, database):
-        self.database = database
-
-    @route('/all', methods=["GET"])
-    def all(self):
-        all_urls = self.database.get_all_urls()
+def init_urls(database):
+    @urls_bp.route('/all', methods=["GET"])
+    def all():
+        all_urls = database.get_all_urls()
         return jsonify(all_urls)
 
-    @route('/check', methods=["POST"])
-    def check(self):
+    @urls_bp.route('/check', methods=["POST"])
+    def check():
         urls = request.get_json()["urls"]
-        return jsonify(self.database.get_urls_exist(urls))
+        return jsonify(database.get_urls_exist(urls))
 
-    @route('/block', methods=["POST"])
-    def block(self):
+    @urls_bp.route('/block', methods=["POST"])
+    def block():
         urls = request.get_json()["urls"]
-        return jsonify(self.database.set_urls(urls))
+        return jsonify(database.set_urls(urls))
 
-    @route('/unblock', methods=["POST"])
-    def unblock(self):
+    @urls_bp.route('/unblock', methods=["POST"])
+    def unblock():
         urls = request.get_json()["urls"]
-        return jsonify(self.database.delete_urls([html.unescape(url) for url in urls]))
+        return jsonify(database.delete_urls([html.unescape(url) for url in urls]))
+
+    return urls_bp
